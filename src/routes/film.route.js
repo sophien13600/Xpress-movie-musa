da7686784f8728c7ca74904d241301fd39d0d5f1
yalcin <yalcinmusa60@gamil.com>
2025-09-14 15:29:02 +0200

    footer style

--- src/routes/film.route.js
+++ src/routes/film.route.js
@@ -6,8 +6,8 @@ const router = express.Router()
 
 router.post('/admin', filmController.saveFilm);
 router.get(['/', '/admin'], filmController.showFilms);
-router.get('/admin/delete/:id', filmController.removeFilm);
-router.post('/admin/update_film/:id', filmController.updateFilm);
+router.get('/admin/delete/film/:id', filmController.removeFilm);
+router.post('/admin/update/film/:id', filmController.updateFilm);
 router.post(['/search', '/favori/search', '/admin/search', '/login/search', '/signup/search'], filmController.searchFilm);
 
 export default router;
