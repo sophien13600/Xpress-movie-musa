c35a5a0683114d0b6fe44a6aeef5827505db21a6
M Yalcin <yalcin@gmail.com>
2025-10-01 14:06:08 +0200

    cors updated

--- index.js
+++ index.js
@@ -13,8 +13,13 @@ import cors from "cors";
 
 const app = express()
 
-app.use(cors());
-
+// app.use
+// (cors());
+app.use(cors({
+    origin: 'https://papaya-gaufre-36ab9d.netlify.app',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+}));
 // configurer la session
 app.use(session({
     secret: 'express-ejs',
