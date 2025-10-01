f70a036b8dcd5bc2d77580b06999852a9beb74aa
M Yalcin <yalcin@gmail.com>
2025-10-01 12:09:04 +0200

    supabase added for deploy

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,11 +3,25 @@ import './App.css'
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
 import AppRoutes from './router/AppRoutes'
+import { useEffect, useState } from "react";
+
+import { createClient } from "@supabase/supabase-js";
 
 
+const supabase = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY);
 
 
 function App() {
+
+
+  const [instruments, setInstruments] = useState([]);
+  useEffect(() => {
+    getInstruments();
+  }, []);
+  async function getInstruments() {
+    const { data } = await supabase.from("instruments").select();
+    setInstruments(data);
+  }
   return (
     <>
       <Header />
